ae3d8f0a690d523abb3ca02f7fa6ae9e6807c98d
Dk Nur Syafiqah Nadiah <21ftt1271@student.pb.edu.bn>
2023-01-31 15:29:59 +0800

    Trying basic OOP

--- testeclipseintegration/src/testeclipseintegration/test/Test.java
+++ testeclipseintegration/src/testeclipseintegration/test/Test.java
@@ -3,6 +3,8 @@ package testeclipseintegration.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("GitHub");
+		ClassA classA = new ClassA("ABCDEF");
+		System.out.println(classA.getAbc());
 	}
 
 }
